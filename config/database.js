83c0d0b81a285d278c2b6b7c8184e79978550207
Julio Cervantes <65304872+JCervantesB@users.noreply.github.com>
2022-12-05 15:32:54 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE'),
-      user: env('PGUSER'),
-      password: env('PGPASSWORD'),
-      ssl: env.bool(false),
+      host: env('DATABASE_HOST', 'postgresql-appsalon.alwaysdata.net'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'appsalon_strapi'),
+      user: env('DATABASE_USERNAME', 'appsalon'),
+      password: env('DATABASE_PASSWORD', 'WebCompleto2022'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
